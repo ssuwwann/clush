ff260fb380e0c3218514da216ddc233961364dc6
swansea <leesuwan89@gmail.com>
2024-12-06 11:53:19 +0900

    init: 프로젝트 초기화

---
+++ back/src/main/java/com/suwan/clush/ClushApplication.java
@@ -0,0 +1,13 @@
+package com.suwan.clush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClushApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ClushApplication.class, args);
+  }
+
+}
---
+++ back/src/test/java/com/suwan/clush/ClushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suwan.clush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClushApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
